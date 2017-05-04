89c7ee9f0ff3701f407436b368d9dc291e3810f1
wangyang59 <wangyang59@baidu.com>
2017-05-04 10:24:09 -0700

    small fix

--- video_prediction/stab_video.py
+++ video_prediction/stab_video.py
@@ -8,7 +8,7 @@ def stablize(out_dir, vide_file):
   os.system( '/home/wangyang59/opencv-3.2.0/samples/cpp/example_cmake/build/opencv_example %s --out-dir=%s' % (vide_file,  out))
 
 def main():
-  data_dir = '/home/wangyang59/Data/ILSVRC2016/Data/VID/snippets/train/ILSVRC2015_VID_train_0001/'
+  data_dir = '/home/wangyang59/Data/ILSVRC2016/Data/VID/snippets/train/ILSVRC2015_VID_train_0002/'
   video_files = os.listdir(data_dir)
   
 #   for h5_file in h5_files[0:10]:
@@ -16,7 +16,21 @@ def main():
   
   
   
-  fun = functools.partial(stablize, "/home/wangyang59/Data/ILSVRC2016_stab/train/")
+  fun = functools.partial(stablize, "/home/wangyang59/Data/ILSVRC2016_stab/train2/")
+  pool = multiprocessing.Pool(20)
+  pool.imap_unordered(fun, [os.path.join(data_dir, video_file) for video_file in video_files], chunksize=10)
+  pool.close()
+  pool.join()
+
+  data_dir = '/home/wangyang59/Data/ILSVRC2016/Data/VID/snippets/train/ILSVRC2015_VID_train_0003/'
+  video_files = os.listdir(data_dir)
+
+#   for h5_file in h5_files[0:10]:
+#     convert_to(os.path.join(h5_dir, h5_file), "/home/wangyang59/Data/ILSVRC2016_tf/train")
+
+
+
+  fun = functools.partial(stablize, "/home/wangyang59/Data/ILSVRC2016_stab/train2/")
   pool = multiprocessing.Pool(20)
   pool.imap_unordered(fun, [os.path.join(data_dir, video_file) for video_file in video_files], chunksize=10)
   pool.close()
